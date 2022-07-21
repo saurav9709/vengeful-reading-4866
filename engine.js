425983a9945c3f2edadb7e0964e5f2e0e989399e
saurav9709 <103635803+saurav9709@users.noreply.github.com>
2022-07-21 20:41:27 +0530

    day-3

---
+++ engine.js
@@ -0,0 +1,12 @@
+let login=document.querySelector("#login");
+let signup=document.querySelector("#signup");
+login.addEventListener("click",loginfun);
+function loginfun(){
+    document.write(55+44);
+
+}
+signup.addEventListener("click",signupfun);
+    function signupfun(){
+        document.querySelector("#easy").style.display="grid"
+    }
+document.write()
